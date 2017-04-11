2a4b64039f6710743d3f7a7c1057957fc9190047
Max Bo <max@maxbo.me>
2017-04-11 13:29:20 +1000

    Add some more stuff to the program

--- Assignment_2_Program.py
+++ Assignment_2_Program.py
@@ -6,14 +6,15 @@ def pretty(i, j, k, r, D):
 A = list(range(1, 12, 2)); print(f"The length of A is {len(A)}")
 B = list(range(5, 17, 1)); print(f"The length of B is {len(B)}")
 C = list(range(6, 19, 3)); print(f"The length of C is {len(C)}")
-# A = list(range(1, 10))
-# B = list(range(1, 10))
-# C = list(range(1, 10))
+# A = [0]
+# B = [1]
+# C = [1]
 D = [EMPTY] * max(len(A), len(B), len(C))
 
 i, j, k, r = 0, 0, 0, 0; print("\nKEY"); pretty("i", "j", "k", "r", D); print()
 
 while ((i != len(A)) and (j != len(B)) and (k != len(C))):
+    # INVARIANT
     assert (set(D[0:r]) == set(A[0:i]) & set(B[0:j]) & set(C[0:k]))
     assert i in range(len(A)) and j in range(len(B)) and k in range(len(C)) and r in range(len(D))
 
@@ -32,6 +33,8 @@ while ((i != len(A)) and (j != len(B)) and (k != len(C))):
         print(f"At A[{i}], B[{j}], and C[{k}], they had the value {A[i]}")
         i, j, k, D[r], r = i + 1, j + 1, k + 1, A[i], r + 1
 
+
+# POSTCONDITION
 assert set( i for i in D if i != EMPTY) == set(A) & set(B) & set(C)
 assert i in range(len(A) + 1) and j in range(len(B) + 1) and k in range(len(C) + 1) and r in range(len(D) + 1)
 
